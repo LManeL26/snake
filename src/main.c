a757a84af1c40b0a08fe6f65855d437b51097403
Mane <manentileonardo06@gmail.com>
2025-08-26 10:40:19 +0200

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "default.h"
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+    UNUSED(argc);
+    UNUSED(argv);
+    printf("Snake game initialization...\n");
+
+    return 0;
+}
